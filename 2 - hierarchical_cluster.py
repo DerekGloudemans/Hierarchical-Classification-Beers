d750f8fd5dda08b0c6c16b51121d80c0cf88dcb0
Derek Gloudemans <35013478+DerekGloudemans@users.noreply.github.com>
2019-02-18 14:16:23 -0600

    Iterative hierarchical cluster works

--- 2 - hierarchical_cluster.py
+++ 2 - hierarchical_cluster.py
@@ -24,9 +24,10 @@ x = np.multiply(x,weights)
 idx = []
 downsample_ratio = 100
 
-include = ['blue moon', 'yazhoo', 'coors', 'miller', 'abita', 'new belgium', 'good people', 'distihl', 'laguanitas', 'dogfish', 'smith and lentz', 'black abbey']
+#include = ['blue moon', 'yazoo', 'coors', 'miller', 'abita', 'new belgium', 'good people', 'distihl', 'laguanitas', 'dogfish', 'smith and lentz', 'black abbey']
+include = ['yazoo']
 for i in range(0,len(names)):
-    if i % downsample_ratio  == 0:
+    if i % (downsample_ratio +1)  == 0:
         idx.append(i)
     else:    
         for item in include:
@@ -58,40 +59,52 @@ for i in active:
         print("On row {} of {}".format(i,np.size(dist,0)))
         
 # at each step:
-        
-#select min non-zero distance
-min_arg = np.argmin(dist)
-i = min_arg // len(dist)
-j = min_arg % len(dist)
+while len(active) > 1:
+    print("Current number of active clusters: {}".format(len(active)))
+    #select min non-zero distance
+    min_arg = np.argmin(dist)
+    i = min_arg // len(dist)
+    j = min_arg % len(dist)
+    
+    #create new node that is weighted average of these locations and add row and column to dist
+    node_list.append(((i,j),dist[i,j], node_list[i][2] + node_list[j][2]))
+    
+    # add new idx to index list
+    active.append(len(node_list)-1)
+    # remove old indices from index list
+    active.remove(i)
+    active.remove(j)
+    
+    #set row and column i and j (removed this round) to inf (so they don't get picked again)
+    dist[i,:] = np.Infinity
+    dist[:,i] = np.Infinity
+    dist[j,:] = np.Infinity
+    dist[:,j] = np.Infinity 
 
-#create new node that is weighted average of these locations and add row and column to dist
-node_list.append(((i,j),dist[i,j], node_list[i][2] + node_list[j][2]))
+    
+    
+    # append new node weighted avg to end of x
+    new_node_x = np.asmatrix(np.divide((np.multiply(x[i,:],node_list[i][2]) + np.multiply(x[j,:],node_list[j][2])), \
+                           (node_list[i][2]+ node_list[j][2])))
+    x = np.concatenate((x,new_node_x),0)
+    
+    # calculate distance to all other nodes (reflect across diagonal)
+    # add row and column to dist, then fill in (technically only need to fill in indices in active)
+    new_row = np.zeros([1, np.size(dist,1)])
+    dist = np.concatenate((dist,new_row),0)
+    new_col = np.zeros([np.size(dist,0), 1])
+    dist = np.concatenate((dist,new_col),1)
+    
+    new_idx = len(node_list)-1
+    for k in range(0,new_idx+1):
+        if k in active:
+            if k == new_idx:
+                dist[new_idx,k] = np.Infinity
+            else:
+                dist[k,new_idx] = np.sqrt(np.sum(np.square(x[k,:] - x[new_idx,:])))
+                dist[new_idx,k] = np.sqrt(np.sum(np.square(x[k,:] - x[new_idx,:])))
+        else:
+            dist[k,new_idx] = np.Infinity
+            dist[new_idx,k] = np.Infinity
 
-# add new idx to index list
-active.append(len(node_list)-1)
-# remove old indices from index list
-active.remove(i)
-active.remove(j)
-
-
-# append new node weighted avg to end of x
-new_node_x = np.asmatrix(np.divide((np.multiply(x[i,:],node_list[i][2]) + np.multiply(x[j,:],node_list[j][2])), \
-                       (node_list[i][2]+ node_list[j][2])))
-x = np.concatenate((x,new_node_x),0)
-
-# calculate distance to all other nodes (reflect across diagonal)
-# add row and column to dist, then fill in (technically only need to fill in indices in active)
-new_row = np.zeros([1, np.size(dist,1)])
-dist = np.concatenate((dist,new_row),0)
-new_col = np.zeros([np.size(dist,0), 1])
-dist = np.concatenate((dist,new_col),1)
-
-new_idx = len(node_list)-1
-for i in active:
-    if i == new_idx:
-        dist[new_idx,i] = np.Infinity
-    else:
-        dist[i,new_idx] = np.sqrt(np.sum(np.square(x[i,:] - x[new_idx,:])))
-        dist[new_idx,i] = np.sqrt(np.sum(np.square(x[i,:] - x[new_idx,:])))
-        
         
